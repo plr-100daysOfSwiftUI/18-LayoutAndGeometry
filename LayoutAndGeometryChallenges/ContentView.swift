376bd77d77abbf2636066082d3207364a9c31f65
Paul Richardson <developer@photographerafoot.com>
2020-11-03 16:56:49 +1100

    Initial Commit

---
+++ LayoutAndGeometryChallenges/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometryChallenges
+//
+//  Created by Paul Richardson on 3/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutAndGeometryChallenges/LayoutAndGeometryChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryChallengesApp.swift
+//  LayoutAndGeometryChallenges
+//
+//  Created by Paul Richardson on 3/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
