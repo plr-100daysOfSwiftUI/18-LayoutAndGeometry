9a33fa50def31ba0a043068d093a375ce8ce2555
Paul Richardson <developer@photographerafoot.com>
2020-11-04 12:59:45 +1100

    CHG: set up to mimic Word Scramble

--- LayoutAndGeometryChallenges/ThirdView.swift
+++ LayoutAndGeometryChallenges/ThirdView.swift
@@ -1,21 +1,28 @@
 	//
-//  ThirdView.swift
-//  LayoutAndGeometryChallenges
-//
-//  Created by Paul Richardson on 4/11/20.
-//
-
-import SwiftUI
-
-struct ThirdView: View {
-    var body: some View {
-        Text("Challenge 3")
-    }
-}
-
-struct ThirdView_Previews: PreviewProvider {
-    static var previews: some View {
-        ThirdView()
-    }
-}
+	//  ThirdView.swift
+	//  LayoutAndGeometryChallenges
+	//
+	//  Created by Paul Richardson on 4/11/20.
+	//
+	
+	import SwiftUI
+	
+	struct ThirdView: View {
+		let words = [
+			"wood", "apple", "mink", "oblong", "obvious", "forget", "race", "yesterday", "leader", "onion", "many", "horse", "beaver", "twirp", "fool"
+		]
+		
+		var body: some View {
+			List(words, id: \.self) {
+				Image(systemName: "\($0.count).circle")
+				Text($0)
+			}
+			.navigationBarTitle(Text("Word Scramble"))
+		}
+	}
+	struct ThirdView_Previews: PreviewProvider {
+		static var previews: some View {
+			ThirdView()
+		}
+	}
 	
