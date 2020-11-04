3cc8e0ed3bd7052da62ca7018fa5457cf838a1bd
Paul Richardson <developer@photographerafoot.com>
2020-11-05 09:56:13 +1100

    CHG: increase number of words

--- LayoutAndGeometryChallenges/SecondView.swift
+++ LayoutAndGeometryChallenges/SecondView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SecondView: View {
 	
 	let words = [
-		"wood", "apple", "mink", "oblong", "obvious", "forget", "race", "yesterday", "leader", "onion", "many", "horse", "beaver", "twirp", "fool"
+		"wood", "apple", "mink", "oblong", "obvious", "forget", "race", "yesterday", "leader", "onion", "many", "horse", "beaver", "twirp", "fool", "heavy", "light", "dark", "shadow", "merry", "funeral", "pyre", "expeditionary"
 	]
 	
 	var body: some View {
