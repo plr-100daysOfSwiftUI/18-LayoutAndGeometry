07c396377b0f93c2558a72aefea23899c0ae31d2
Paul Richardson <developer@photographerafoot.com>
2020-11-05 13:43:17 +1100

    CHG: Put a second GeometryReader inside the scroll view; put a frame modifier on it to stop it being greedy; calculate  a scaling factor based on the relative minY of the VStack

--- LayoutAndGeometryChallenges/FirstView.swift
+++ LayoutAndGeometryChallenges/FirstView.swift
@@ -9,21 +9,30 @@ import SwiftUI
 
 struct FirstView: View {
 	var body: some View {
-		GeometryReader { geometry in
+		GeometryReader { outer in
 			ScrollView(.vertical) {
-				VStack {
-					Image("apollo11")
-						.resizable()
-						.scaledToFit()
-						.frame(maxWidth: geometry.size.width * 0.65)
-						.padding(.top)
+				GeometryReader { inner in
 					
-					Text("Change project 8 (Moonshot) so that when you scroll down in MissionView the mission badge image gets smaller. It doesn’t need to shrink away to nothing – going down to maybe 80% is fine.")
-						.padding()
+					let minY = inner.frame(in: .global).minY - outer.frame(in: .global).minY
+					let factor = (100.0 + minY) / 100
+					let scale = factor < 0.6 ? 0.6 : factor
 					
-					Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-						.padding()
+					VStack {
+						Image("apollo11")
+							.resizable()
+							.scaledToFit()
+							.frame(maxWidth: outer.size.width * 0.65 * scale)
+							.padding(.top)
+						
+						Text("Change project 8 (Moonshot) so that when you scroll down in MissionView the mission badge image gets smaller. It doesn’t need to shrink away to nothing – going down to maybe 80% is fine.")
+							.padding()
+						
+						Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+							.padding()
+					}
 				}
+				.frame(width: outer.size.width, height: outer.size.height)
+				
 			}
 		}
 		.navigationBarTitle(Text("Apollo 11"), displayMode: .inline)
